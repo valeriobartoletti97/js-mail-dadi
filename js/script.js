03d64ee2c5ec7bd5b6b8c540b3fafc31eb97b82d
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-12 17:29:47 +0200

    setting dice game

--- js/script.js
+++ js/script.js
@@ -1,15 +1,36 @@
 const validEmail = [
     'pippo@email.it',
-    'pluto@email.it'
+    'pluto@email.it'dadi immaginedice game
 ];
-const results = [1, 2, 3, 4, 5, 6];
+/* const results = [1, 2, 3, 4, 5, 6]; */
 const alert = document.getElementById('alert');
 const correct = document.getElementById('correct');
 const emailBtn = document.querySelector('.btn-success');
-
+const gameBtn = document.getElementById('game-btn')
+const game = document.getElementById('game')
+const result = document.getElementById('result')
+console.log(gameBtn)
 emailBtn.addEventListener("click", function(){
-    const email = document.getElementById('email').value;
+    let email = document.getElementById('email').value;
+    let found = false;
     for(let i = 0; i <= validEmail.length; i++){
-        
+      let currentName = validEmail[i]; 
+      if(currentName/* .toLocaleLowerCase() */ === email/* .toLocaleLowerCase() */){
+        found = true
+      }
+    }
+    if (found === false){
+       correct.classList.add('d-none')
+       alert.classList.remove('d-none')
+    } else {
+       alert.classList.add('d-none')
+       correct.classList.remove('d-none')
+    }
+})
+gameBtn.addEventListener("click", function(){
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1; 
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    if(randomNumber1 === randomNunber2){
+      
     }
 })
