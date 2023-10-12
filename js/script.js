488e3ee442d177a18e7865ec8e2a16addfc2c9e0
valeriobartoletti97 <valerio.bartoletti97@gmail.com>
2023-10-12 16:05:10 +0200

    working on js code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const validEmail = [
     'pippo@email.it',
     'pluto@email.it'
-]
-const results = [1, 2, 3, 4, 5, 6]
-console.log(validEmail, results)
+];
+const results = [1, 2, 3, 4, 5, 6];
+const alert = document.getElementById('alert');
+const correct = document.getElementById('correct');
+const emailBtn = document.querySelector('.btn-success');
+
+emailBtn.addEventListener("click", function(){
+    const email = document.getElementById('email').value;
+    for(let i = 0; i <= validEmail.length; i++){
+        
+    }
+})
